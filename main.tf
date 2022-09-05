ec5b21b8a2c0e239efbc503b0a0091c47c7226bd
anand944 <anandkumarkumbaji07@gmail.com>
2022-09-05 17:27:12 +0530

    trird

--- main.tf
+++ main.tf
@@ -4,7 +4,7 @@ resource "azurerm_resource_group" "rg" {
  
 }
 
-resource "azurerm_service_plan" "asp" {
+resource "azurerm_service_plan" "azureserviceplan" {
   name                = "asp07"
   resource_group_name = azurerm_resource_group.rg.name
   location            = azurerm_resource_group.rg.location
@@ -17,11 +17,11 @@ resource "azurerm_service_plan" "asp" {
   }
   
 }
-resource "azurerm_app_service" "as" {
+resource "azurerm_app_service" "azureappservice" {
   name                = "as07"
   location            = azurerm_resource_group.rg.location
   resource_group_name = azurerm_resource_group.rg.name
-  app_service_plan_id = azurerm_app_service_plan.asp.id
+  app_service_plan_id = azurerm_app_service_plan.azureserviceplan.id
 
   site_config {
     dotnet_framework_version = "v4.0"
